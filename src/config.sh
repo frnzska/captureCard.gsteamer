ac7cb795da250396da82331c4fcd117ca7975337
frnzska <minziska@googlemail.com>
2018-06-27 16:32:23 +0200

    config

---
+++ src/config.sh
@@ -0,0 +1,19 @@
+#Date-Format (for filenames
+now=$(date +"%Y_%m_%d_%H:%M:%S")
+
+#card 0 settings
+filenameCard0=($now'_card0''.mpeg')
+bitrateCard0vid=5000000
+bitrateCard0aud=192000
+modeCard0="0"
+previewWidthCard0=600
+previewHeightCard0=400
+
+
+#card 1 settings
+filenameCard1=($now'_card1''.mpeg')
+bitrateCard1vid=5000000
+bitrateCard1aud=192000
+modeCard1="0"
+previewWidthCard1=600
+previewHeightCard1=400
